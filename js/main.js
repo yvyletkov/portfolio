dd3388055e59d0d9d12a93136fe3cffd44685472
yavyletkov <yavyletkov@ya.ru>
2020-07-31 18:32:23 +0300

    bg img fix

--- js/main.js
+++ js/main.js
@@ -17,11 +17,18 @@ function menu() {
     burgerMenu.addEventListener("click", () => {
         menuInfo.classList.toggle("info_active");
         burgerMenu.classList.toggle("toggle");
-        // if (menuInfo.classList.contains("info_active")) {
-        //     video.style.display = 'none';
-        // } else {
-        //     video.style.display = '';
-        // }
+        if (menuInfo.classList.contains("info_active")) {
+            video.style.display = 'none';
+        } else {
+            video.style.display = '';
+        }
     });
 }
+
+
+if(window.matchMedia('(max-width: 1000px)').matches){
+    document.querySelector(".stage__img-webp").srcset = "./img/me-small.webp"
+    document.querySelector(".stage__img").src = "./img/me-small.png"
+}
+
 menu();
